dba8f923ba77ed364fe963a70acd31e05db46505
Chris Lenk <clenk@mitre.org>
2018-01-16 10:19:26 -0500

    Use confd w/ msweb Dockerfile, more config options

--- docker/distributed/mscan_web.sh
+++ docker/distributed/mscan_web.sh
@@ -3,5 +3,8 @@
 # Run confd to set configuration
 confd -onetime -backend env
 
+# Give apache ownership of the multiscanner directory
+chown -R daemon:daemon /opt/multiscanner
+
 # Run Docker CMD
 bash -c "$@"
