4a602d7e99a0a10e020fa59e8a21739d9c9c4581
Chris Lenk <clenk@mitre.org>
2018-01-25 16:33:41 -0500

    Fix distributed docker-compose file

--- docker/distributed/mscan_rest.sh
+++ docker/distributed/mscan_rest.sh
@@ -9,8 +9,12 @@ sed -i -r "s/Listen 80/Listen $MS_API_PORT/" /usr/local/apache2/conf/httpd.conf
 # Set CORS regex
 sed -i -r "s;\{\{ ms_api_cors_regex \}\};$MS_API_CORS_REGEX;g" /usr/local/apache2/conf/httpd.conf
 
+# Create sample upload directory
+mkdir $MS_UPLOAD_DIR
+
 # Give apache ownership of the multiscanner directory
 chown -R daemon:daemon /opt/multiscanner
+chown -R daemon:daemon $MS_UPLOAD_DIR
 
 # Run Docker CMD
 bash -c "$@"
